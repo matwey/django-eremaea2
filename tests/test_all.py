2a3d1ab4252b2316cfbe66b7cc41d2d29c65c164
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2016-03-28 19:46:01 +0300

    Initial commit

---
+++ tests/test_all.py
@@ -0,0 +1,5 @@
+from tests.collection import CollectionTest
+from tests.snapshot import SnapshotTest
+from tests.retention import RetentionPolicyTest
+
+__all__ = ['CollectionTest', 'SnapshotTest', 'RetentionPolicyTest']
