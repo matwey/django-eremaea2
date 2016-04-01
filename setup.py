10f7b30554a2961d1bc84f87e1d0e475be0c254a
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2016-04-01 13:55:11 +0300

    Version 2.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name='django-eremaea2',
-	version='2.0.0',
+	version='2.0.1',
 	packages=['eremaea','eremaea.ctl'],
 	scripts=['eremaeactl'],
 	include_package_data=True,
