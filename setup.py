ab5a1e8a5f8536f2cbd2d0135f0b8a2f7823f56f
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2016-04-10 15:00:11 +0300

    Version 2.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name='django-eremaea2',
-	version='2.0.4',
+	version='2.0.5',
 	packages=['eremaea','eremaea.ctl','eremaea.migrations'],
 	entry_points={'console_scripts': [
 		'eremaeactl = eremaea.ctl.commandline:execute_from_commandline',
