09d0e3e20d006253c131b61a9dbd5c4ca207b502
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2017-08-06 16:50:12 +0300

    Use DefaultRouter() instead of SimpleRouter()

--- eremaea/urls.py
+++ eremaea/urls.py
@@ -1,8 +1,9 @@
 from eremaea import views
 from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+router = DefaultRouter()
 router.register(r'collections', views.CollectionViewSet)
 router.register(r'snapshots', views.SnapshotViewSet)
 router.register(r'retention_policies', views.RetentionPolicyViewSet, base_name='retention_policy')
