b0bdeca50e58e8de712d58cebb89a889c2d355c1
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2024-10-03 16:44:15 +0300

    Use click instead of deprecated cmdln

--- src/eremaea/ctl/commandline.py
+++ src/eremaea/ctl/commandline.py
@@ -1,70 +1,68 @@
-import cmdln
+import click
 import time
 import sys
 from eremaea.ctl.file import create_stream
 from eremaea.ctl.client import Client
 from django.utils.dateparse import parse_duration
 
-class CommandLine(cmdln.Cmdln, object):
-	name = "eremaeactl"
 
-	def get_optparser(self):
-		parser = cmdln.Cmdln.get_optparser(self)
-		parser.add_option("-A", dest="api_endpoint", help="HTTP REST API endpoint URL", default="http://127.0.0.1/eremaea")
-		parser.add_option("-T", dest="api_token", help="HTTP Token Authorization")
-		return parser
-	def precmd(self, argv):
-		kwargs = {}
-		if self.options.api_token:
-			kwargs['token'] = self.options.api_token
-		self.client = Client(self.options.api_endpoint, **kwargs)
-		return super(CommandLine, self).precmd(argv)
-	def postcmd(self, argv):
-		del self.client
+@click.group()
+@click.option("-A", "--endpoint", "endpoint", show_default=True, default="http://127.0.0.1/eremaea", help="HTTP REST API endpoint URL")
+@click.option("-T", "--token", "token", help="HTTP Token Authorization")
+@click.pass_context
+def cli(ctx, endpoint, token):
+	kwargs = {}
+	if token is not None:
+		kwargs['token'] = token
+	ctx.obj = {'client': Client(endpoint, **kwargs)}
 
-	@cmdln.alias("up")
-	@cmdln.option("-q", "--quite", action="store_true", help="be quite")
-	@cmdln.option("-r", dest="retention_policy", help="specify retention policy (optional)")
-	def do_upload(self, subcmd, opts, file, collection):
-		"""${cmd_name}: upload file to storage
+@cli.command()
+@click.option("-q", "--quite", "quite", is_flag=True, help="be quite")
+@click.option("-r", "--retention_policy", "retention_policy", help="specify retention policy (optional)")
+@click.argument("filename")
+@click.argument("collection")
+@click.pass_context
+def upload(ctx, quite, retention_policy, filename, collection):
+	client = ctx.obj['client']
 
-		${cmd_usage}
-		${cmd_option_list}
-		"""
-		self.client.upload(next(create_stream(file)), collection, opts.retention_policy)
-	@cmdln.option("--all", dest="all", action="store_true", help="purge all retention policies")
-	def do_purge(self, subcmd, opts, *retention_policies):
-		"""${cmd_name}: purge retention policies
+	client.upload(next(create_stream(filename)), collection, retention_policy)
 
-		${cmd_usage}
-		${cmd_option_list}
-		"""
-		if opts.all:
-			retention_policies = self.client.retention_policies()
-		if not retention_policies and not opts.all:
-			CommandLine.do_purge.optparser.print_help()
-		for x in retention_policies:
-			self.client.purge(x)
-	@cmdln.option("-q", "--quite", action="store_true", help="be quite")
-	@cmdln.option("-i", "--interval", help="pull interval", default="1:00")
-	@cmdln.option("-r", dest="retention_policy", help="specify retention policy (optional)")
-	def do_pull(self, subcmd, opts, file, collection):
-		"""${cmd_name}: pull image
+@cli.command()
+@click.option("--all", "all", is_flag=True, help="purge all retention policies")
+@click.argument("retention_policies", nargs=-1)
+@click.pass_context
+def purge(ctx, all, retention_policies):
+	client = ctx.obj['client']
 
-		${cmd_usage}
-		${cmd_option_list}
-		"""
-		duration = parse_duration(opts.interval)
-		stream = create_stream(file)
-		while True:
-			try:
-				self.client.upload(next(stream), collection, opts.retention_policy)
-			except Exception as e:
-				if not opts.quite:
-					sys.stderr.write(str(e) + "\n")
-					sys.stderr.flush()
-			time.sleep(duration.total_seconds())
+	if retention_policies and all:
+		raise click.BadParameter("No additional agruments are allowed for --all")
 
-def execute_from_commandline(argv=None):
-	cmd = CommandLine()
-	cmd.main(argv)
+	if all:
+		retention_policies = client.retention_policies()
+	for x in retention_policies:
+		client.purge(x)
+
+@cli.command()
+@click.option("-q", "--quite", "quite", is_flag=True, help="be quite")
+@click.option("-i", "--interval", "interval", default="1:00", help="pull interval", show_default=True)
+@click.option("-r", "--retention_policy", "retention_policy", help="specify retention policy (optional)")
+@click.argument("filename")
+@click.argument("collection")
+@click.pass_context
+def pull(ctx, quite, interval, retention_policy, filename, collection):
+	client = ctx.obj['client']
+
+	duration = parse_duration(interval)
+	stream = create_stream(filename)
+	while True:
+		try:
+			client.upload(next(stream), collection, retention_policy)
+		except Exception as e:
+			if not quite:
+				sys.stderr.write(str(e) + "\n")
+				sys.stderr.flush()
+		time.sleep(duration.total_seconds())
+
+
+if __name__ == '__main__':
+	cli()
