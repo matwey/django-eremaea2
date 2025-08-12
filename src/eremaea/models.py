2988502c02b3c76c5cb9fc5965ca9c3f08245617
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2025-08-12 12:18:11 +0300

    Use models.SlugField for names of collection and retention_policy

--- src/eremaea/models.py
+++ src/eremaea/models.py
@@ -74,11 +74,11 @@ class Snapshot(models.Model):
 		get_latest_by = 'date'
 
 class Collection(models.Model):
-	name = models.CharField(max_length=256, blank=False, unique=True, db_index=True)
+	name = models.SlugField(max_length=256, blank=False, unique=True, db_index=True)
 	default_retention_policy = models.ForeignKey('RetentionPolicy', on_delete=models.PROTECT)
 
 class RetentionPolicy(models.Model):
-	name = models.CharField(max_length=256, blank=False, null=False, unique=True, db_index=True)
+	name = models.SlugField(max_length=256, blank=False, null=False, unique=True, db_index=True)
 	duration = models.DurationField(blank=False, null=False)
 
 	class Meta:
